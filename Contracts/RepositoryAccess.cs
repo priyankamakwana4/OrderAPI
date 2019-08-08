41ebfdc37b552d8ef297fd74cc54b9e1b13341b9
priyankamakwana4 <40364607+priyankamakwana4@users.noreply.github.com>
2019-08-08 14:23:22 -0700

    Add files via upload

---
+++ Contracts/RepositoryAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cassandra;
+
+namespace Contracts
+{
+    public class RepositoryAccess : IRepositoryAccess
+    {
+        private Cluster cluster;
+        private ISession session;
+
+        public RepositoryAccess()
+        {
+            cluster = Cluster.Builder().WithCredentials("admin", "admin").AddContactPoint("172.30.231.236").Build();
+            session = cluster.Connect("orderdb");
+        }
+        public string PostOrder(Order orderobj)
+        {
+            try
+            {
+                orderobj.Id = Guid.NewGuid();
+                string strCQL = "insert into orders (id, item, quantity, price) values (?,?,?,?)";
+                PreparedStatement preparedStatement = session.Prepare(strCQL);
+                BoundStatement boundStatement = preparedStatement.Bind(orderobj.Id, orderobj.Item, orderobj.Quantity, orderobj.Price);
+                session.Execute(boundStatement);
+                return "true";
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
